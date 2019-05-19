f1c6693126e2b2884a6adf0fbf3f35f68ee94e4d
Albert Lacambra Basil <albert@lacambra.tech>
2019-05-19 15:39:26 +0200

    json

--- json-patch-and-pointer/src/test/java/json_patch_and_pointer/PatchAndPointer.java
+++ json-patch-and-pointer/src/test/java/json_patch_and_pointer/PatchAndPointer.java
@@ -8,6 +8,18 @@ import javax.json.*;
 public class PatchAndPointer {
 
   @Test
+  void oldVsNewWay() {
+
+    JsonObject jsonObject = Json.createReader(getClass().getClassLoader().getResourceAsStream("example.json")).readObject();
+
+    String nameWithObject = jsonObject.getJsonArray("user_mentions").getJsonObject(0).getString("name");
+    String nameWithPointer = ((JsonString) Json.createPointer("/user_mentions/0/name").getValue(jsonObject)).getString();
+
+
+    Assertions.assertEquals(nameWithObject, nameWithPointer);
+  }
+
+  @Test
   void pointerDemo() {
 
     JsonObject example = Json.createReader(getClass().getClassLoader().getResourceAsStream("example.json")).readObject();
@@ -42,14 +54,29 @@ public class PatchAndPointer {
      * Check if a structures contains a path
      */
     Assertions.assertTrue(Json.createPointer("/id").containsValue(example));
+    Assertions.assertTrue(Json.createPointer("/user_mentions/0/indices/0").containsValue(example));
     Assertions.assertTrue(Json.createPointer("/user_mentions/0/indices/1").containsValue(example));
-    Assertions.assertFalse(Json.createPointer("/user_mentions/0/indices/3").containsValue(example));
+    Assertions.assertFalse(Json.createPointer("/user_mentions/0/indices/2").containsValue(example));
 
     /**
      * Add some value
      */
-    JsonObject extendedExample = Json.createPointer("/timestamp").add(example, Json.createValue(1234));
+    JsonObject extendedExample = Json.createPointer("/timestamp").add(example, Json.createValue(System.currentTimeMillis()));
+    extendedExample = Json.createPointer("/user_mentions/0/indices/2").add(extendedExample, Json.createValue(30));
+
     Assertions.assertTrue(Json.createPointer("/timestamp").containsValue(extendedExample));
+    Assertions.assertEquals(30, ((JsonNumber) Json.createPointer("/user_mentions/0/indices/2").getValue(extendedExample)).intValue());
+
+    //Replace
+    example = Json.createPointer("/id").replace(example, Json.createValue(2));
+    Assertions.assertEquals(2, example.getInt("id"));
+
+    example = Json.createPointer("/user_mentions/0/indices/1").replace(example, Json.createValue(9999));
+    Assertions.assertEquals(9999, ((JsonNumber) Json.createPointer("/user_mentions/0/indices/1").getValue(example)).intValue());
+
+    //Remove
+    example = Json.createPointer("/id").remove(example);
+    Assertions.assertFalse(example.containsKey("id"));
 
 
   }
